82f257c0d4b4edfaac6646c8e41703393e24f100
Charith <charithmaddoju27@gmail.com>
2022-09-03 07:36:43 +0530

    two input responsive calculator

--- myscript.js
+++ myscript.js
@@ -68,7 +68,11 @@ buttons.forEach((button) => {
             flag = 1;
         }
 
-        text.textContent += button.value;    
+        text.textContent += button.value; 
+
+        if(input1 != '' && choice != '' && input2 != ''){
+        result.textContent = operate(choice,parseInt(input1), parseInt(input2));   
+        }
         
     })
 })
@@ -77,6 +81,11 @@ console.log(input2)
 
 equals.addEventListener('click', () => {
     result.textContent = operate(choice,parseInt(input1), parseInt(input2));
+    result.style.color = "black";
+    result.style.fontSize = "30px";
+    text.style.fontSize = "20px";
+    text.style.color = "grey";
+    
 })
     
  reset.addEventListener('click', () => {
@@ -86,6 +95,10 @@ equals.addEventListener('click', () => {
      input2 = '';
      choice = '';
      flag = 0;
+     result.style.color = "grey";
+    result.style.fontSize = "20px";
+    text.style.fontSize = "30px";
+    text.style.color = "black";
  })
 
 
@@ -93,4 +106,18 @@ equals.addEventListener('click', () => {
      const someString = text.textContent;
      const newString = someString.slice(0,-1);
      text.textContent = newString;
+     if(input2 != ''){
+        input2 = input2.slice(0,-1);
+        result.textContent = operate(choice,parseInt(input1), parseInt(input2));     
+     }
+     else{
+        
+        choice === '';
+        flag = 0;
+        result.textContent = '';
+     }
+     result.style.color = "grey";
+    result.style.fontSize = "20px";
+    text.style.fontSize = "30px";
+    text.style.color = "black";
  })
